c008f4ec0912d402f95c1bb548d51446a728c866
William <78174636+William-1212@users.noreply.github.com>
2021-02-20 08:59:49 -0300

    Update
    Adicionado

--- Models/AgendaModel.cs
+++ Models/AgendaModel.cs
@@ -5,12 +5,14 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Diagnostics;
 using Microsoft.AspNetCore;
 using Agenda.Models;
 
 namespace Agenda.Models
 {
+    [Table("Aluguel")]
     public class AgendaModel
     {
         [Key]
